08eca5456cf005787fd7f514ca1992f8d8719daf
Dharmit <dharmitsathvara29@gmail.com>
2026-09-24 10:49:26 +0530

    Move all code files into Dharmit_Sathvara folder, keeping only Dharmit_Sathvara and README.md in root

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, registerStaff, login, profile } = require('../controllers/authController');
-const authenticate = require('../middleware/auth');
-const router = express.Router();
-router.post('/register', register);
-router.post('/register-staff', registerStaff);
-router.post('/login', login);
-router.get('/profile', authenticate, profile);
-module.exports = router;
